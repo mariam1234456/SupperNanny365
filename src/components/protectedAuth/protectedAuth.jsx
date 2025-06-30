3cf47f983cd5e6599c9f2cd0bfb0d19670823865
mariam1234456 <mohamadbadr222@gmail.com>
2025-06-30 22:33:16 +0200

    last last

--- src/components/protectedAuth/protectedAuth.jsx
+++ src/components/protectedAuth/protectedAuth.jsx
@@ -1,8 +1,8 @@
 import { Navigate } from "react-router-dom";
-import style from "./ProtectedAuth.module.css"
+import style from "./protectedAuth.module.css"
 import React from 'react'
 
-export default function ProtectedAuth(props) {
+export default function protectedAuth(props) {
   if (localStorage.getItem("accessToken") && localStorage.getItem("refreshToken")){
     return <Navigate to="/todo"></Navigate>
   }else{
